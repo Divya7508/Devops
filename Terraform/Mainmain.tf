9ff1cfa2b95a54301798629f95f647fd698cd16b
DIVYA <142962877+Divya7508@users.noreply.github.com>
2024-01-12 17:48:52 +0530

    Mainmain.tf

--- Terraform/Mainmain.tf
+++ Terraform/Mainmain.tf
@@ -19,3 +19,14 @@ resource "aws_s3_bucket_versioning" "s3_tk_versioning" {
   }
   depends_on = [aws_s3_bucket_server_side_encryption_configuration.s3_tk]
 }
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "s3_tumkur-locking-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockId"
+  
+  attribute {
+    name = "LockId"
+    type = "S"
+  }
+}
+
