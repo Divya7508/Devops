2f6b56ec961d8192fd9f35531bc8a64b0913bacc
DIVYA <142962877+Divya7508@users.noreply.github.com>
2024-01-12 17:41:02 +0530

    Mainmain.tf

---
+++ Terraform/Mainmain.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "s3_tumkur" {
+  bucket = "s3-ilkal"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_tk" {
+  bucket = aws_s3_bucket.s3_tumkur.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+  
+resource "aws_s3_bucket_versioning" "s3_tk_versioning" {
+  bucket = aws_s3_bucket.s3_tumkur.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+  depends_on = [aws_s3_bucket_server_side_encryption_configuration.s3_tk]
+}
