da1cbce35dfeb84ddecbfb7b90a84485eb9b3c88
DIVYA <142962877+Divya7508@users.noreply.github.com>
2024-01-13 11:42:42 +0530

    local variables- Main.tf
    used variables

--- Terraform/Main.tf
+++ Terraform/Main.tf
@@ -1,8 +1,12 @@
-resource "aws_s3_bucket" "s3_backend" {
-  bucket = "s3-backend-terra "
+locals {
+    hash_key_local = "LockID"
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "s3_sse" {
+resource "aws_s3_bucket" "s3_backend" {
+  bucket = var.s3_bucket_name
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_backend_sse" {
   bucket = aws_s3_bucket.s3_backend.id
   rule {
     apply_server_side_encryption_by_default {
@@ -11,11 +15,21 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_sse" {
   }
 }
 
-  
-resource "aws_s3_bucket_versioning" "s3_sse_versioning" {
+resource "aws_s3_bucket_versioning" "s3_backend_sse_versioning" {
   bucket = aws_s3_bucket.s3_backend.id
   versioning_configuration {
     status = "Enabled"
   }
-  depends_on = [aws_s3_bucket_server_side_encryption_configuration.s3_sse]
+  depends_on = [ aws_s3_bucket_server_side_encryption_configuration.s3_backend_sse ]
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = var.dynamodb_name
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = local.hash_key_local
+
+  attribute {
+    name = local.hash_key_local
+    type = "S"
+  }
 }
