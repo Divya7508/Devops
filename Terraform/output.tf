a8f77056933cb12f03f33be5f89083035a37278f
DIVYA <142962877+Divya7508@users.noreply.github.com>
2024-01-13 12:13:45 +0530

    output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,10 +1,10 @@
-output "s3_backend_id" {
-  value = aws_s3_bucket.s3_backend.id
+output "s3_tumkur_id" {
+  value = aws_s3_bucket.s3_tumkur.id
   description = "The name of the bucket."
 }
 
 output "s3_backend_hosted_zone" {
-  value = aws_s3_bucket.s3_backend.hosted_zone_id
+  value = aws_s3_bucket.s3_tumkur.hosted_zone_id
   description = "The Hosted zone of the bucket."
 }
 
@@ -12,3 +12,9 @@ output "dynamodb_name" {
   value = aws_dynamodb_table.basic-dynamodb-table.name
   description = "The name of the bucket."
 }
+
+
+# output will be after terraform apply
+# dynamodb_name = "s3_tumkur-locking-table"
+# s3_backend_hosted_zone = "Z11RGJOFQNVJUP"
+# s3_tumkur_id = "s3-ilkal"
